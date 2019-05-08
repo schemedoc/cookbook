64c4d6bf6abce20b027fbb24390ed0d517b05c9c
Lassi Kortela <lassi@lassi.io>
2019-05-08 12:37:42 +0300

    Remove unused function

--- scrape.rkt
+++ scrape.rkt
@@ -18,9 +18,6 @@
 (define (whitespace-cleanup str)
   (regexp-replace* #px"(?m:\\s+$)" str ""))
 
-(define (newline-join strings)
-  (string-append (string-join strings "\n") "\n"))
-
 (define (add-final-newline s)
   (if (= 0 (string-length s)) s (string-append s "\n")))
 
