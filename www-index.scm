64e7b6881c55b65051282c14d99ffb7e2763b5b5
Lassi Kortela <lassi@lassi.io>
2021-07-31 13:01:55 +0300

    Move index S-exp into its own file

---
+++ www-index.scm
@@ -0,0 +1,26 @@
+(("Pairs and lists"
+  "find-the-index-of-an-element-in-a-list"
+  "finding-the-most-frequent-element-in-list"
+  "removing-duplicates-from-a-list"
+  "removing-item-from-a-list"
+  "select-random-elements-from-a-list-minimising-unwanted-duplicates"
+  "split-list-into-groups-of-n-elements"
+  "split-list-into-groups-that-are-equal-judging-by-a-procedure")
+
+ ("Association lists, property lists, and hash tables"
+  "creating-subset-of-an-alist")
+
+ ("Strings"
+  "convert-any-value-to-string"
+  "join-list-of-strings-with-delimiter"
+  "remove-whitespace-from-string"
+  "split-a-string")
+
+ ("Numbers"
+  "convert-integer-to-list-of-digits")
+
+ ("Time"
+  "format-unix-timestamp")
+
+ ("File system"
+  "find-matching-files-in-directory-tree"))
--- www.scm
+++ www.scm
@@ -68,28 +68,7 @@
            (title (or (page-title-from-sxml sxml) stem)))
       (make-recipe stem title sxml))))
 
-(define groups-template
-  '(("Pairs and lists"
-     "find-the-index-of-an-element-in-a-list"
-     "finding-the-most-frequent-element-in-list"
-     "removing-duplicates-from-a-list"
-     "removing-item-from-a-list"
-     "select-random-elements-from-a-list-minimising-unwanted-duplicates"
-     "split-list-into-groups-of-n-elements"
-     "split-list-into-groups-that-are-equal-judging-by-a-procedure")
-    ("Association lists, property lists, and hash tables"
-     "creating-subset-of-an-alist")
-    ("Strings"
-     "convert-any-value-to-string"
-     "join-list-of-strings-with-delimiter"
-     "remove-whitespace-from-string"
-     "split-a-string")
-    ("Numbers"
-     "convert-integer-to-list-of-digits")
-    ("Time"
-     "format-unix-timestamp")
-    ("File system"
-     "find-matching-files-in-directory-tree")))
+(define groups-template (with-input-from-file "www-index.scm" read))
 
 (define group-title car)
 (define group-recipes cdr)
